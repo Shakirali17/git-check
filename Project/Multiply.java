71dae38e0ef70386824fa4bc0b88bf5494268d19
Shakirali <shakirali0075@gmail.com>
2025-10-21 21:50:48 +0530

    done

---
+++ Project/Multiply.java
@@ -0,0 +1,8 @@
+package Project;
+
+public class Multiply {
+  static int mul(int a,int b){
+       int c=a*b;
+        return c;
+    }
+}
---
+++ Project/first.java
@@ -0,0 +1,12 @@
+package Project;
+// import Project.Multiply;
+
+public class first{
+    public static void main(String[] args) {
+        int a=3,b=4;
+        int c=5;
+
+        // System.out.println("sum:"+(a+b));
+        System.out.println(Multiply.mul(a,b));
+    }
+}
